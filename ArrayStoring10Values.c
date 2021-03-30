b51f0043d8b08833ffb2deb8f6b98aa5e6727f1b
shane52 <2020a1r161@mietjammu.in>
2021-03-30 15:11:04 +0530

    Array storing values

--- ArrayStoring10Values.c
+++ ArrayStoring10Values.c
@@ -1,11 +1,11 @@
-#include<stdio.h>
-int main()
-{  int arr[10],i;
-for(i=0;i<10;i++)
+#include<stdio.h> //input output header file
+int main()        //function of the main body
+{  int arr[10],i; //selecting data type and variable declaratio
+for(i=0;i<10;i++) //for loop statement
 {
-printf("Enter numbers which u want to store in this array;\n");
-scanf("%d",&arr[i]);
-
+printf("Enter numbers which u want to store in this array;\n"); //printf function is used to display output
+scanf("%d",&arr[i]);                                            //Scanf function is used to take input from user
+//overall an array is made which can store 10 values that are taken by user using for loop
 }
-
+return 0;          //Return value
 }
