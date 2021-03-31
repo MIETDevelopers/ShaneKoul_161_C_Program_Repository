cd6c0eb90ffd9fa1acd7cfdbb02b54c21eabdca9
shane52 <2020a1r161@mietjammu.in>
2021-03-31 17:32:57 +0530

    Assignment_37

---
+++ Assignment_37.c
@@ -0,0 +1,26 @@
+#include <stdio.h>//Input output standarad header file
+#include<stdlib.h> //This libraray is used because we have used exit function
+int main()        //function of the main body
+{
+	char name[50];  //selecting data type and variable declaration caple of holding 50 values
+	int marks,i,n;  //selecting data type and variable declaration
+	printf("Enter number of students: ");  //printf function calling 
+	scanf("%d",&n); 						//scanf function used for taking input from iser
+	FILE *fptr;                            //file name and pointer variable declaration
+	fptr=(fopen("C:\\student.txt","w"));  //fopen is used to open any csv file and read data inside it
+	if(fptr==NULL) 
+	{
+		printf("Opened");
+		exit(1);
+	}
+	for (i=0;i<n;++i)
+	{
+		printf("For student%d\nEnter name: ",i+1);
+		scanf("%s",name);
+		printf("Enter marks: ");
+		scanf("%d",&marks);
+		fprintf(fptr,"\nName: %s \nMarks=%d \n",name,marks);
+	}
+	fclose(fptr);
+	return 0;
+}
