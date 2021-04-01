fa61993ad286df67ad4ce509c21cbc89e4654a3e
shane52 <2020a1r161@mietjammu.in>
2021-04-01 13:58:55 +0530

    Reading data from file

---
+++ ReadingDataFromCsv.c.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+	FILE *fp;
+	char ch;
+	fp= fopen("D:\\a.txt","r");
+	while(1)
+	{
+		ch= fgetc(fp);
+		if(ch==EOF)
+		break;
+		printf("%c",ch);
+	}
+	printf("\n");
+	fclose(fp);
+	return 0;
+}
